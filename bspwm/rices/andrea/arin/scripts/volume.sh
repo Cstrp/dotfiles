85b88a374d1cadde70f60257aed45d3845e0636f
Cstrp <valery.nosareu@gmail.com>
2023-08-13 22:29:51 +0300

    change picom conf | starship | other

--- bspwm/rices/andrea/arin/scripts/volume.sh
+++ bspwm/rices/andrea/arin/scripts/volume.sh
@@ -4,21 +4,21 @@
 
 SINK=$(pactl list short sinks | sed -e 's,^\([0-9][0-9]*\)[^0-9].*,\1,' | head -n 1)
 
-percentage () {
+percentage() {
   local val=$(echo $1 | tr '%' ' ' | awk '{print $1}')
 }
 
-get_percentage () {
+get_percentage() {
   local muted=$(pamixer --get-volume-human)
   if [[ $muted == 'muted' ]]; then
     echo 0%
   else
-    per=$(pactl list sinks | grep '^[[:space:]]Volume:' | head -n $(( $SINK + 1 )) | tail -n 1 | sed -e 's,.* \([0-9][0-9]*\)%.*,\1,')
+    per=$(pactl list sinks | grep '^[[:space:]]Volume:' | head -n $(($SINK + 1)) | tail -n 1 | sed -e 's,.* \([0-9][0-9]*\)%.*,\1,')
     echo "${per}%"
   fi
 }
 
-get_icon () {
+get_icon() {
   local vol=$(get_percentage)
   if [[ $vol == "0%" ]]; then
     echo "images/icons/volume/mute.png"
@@ -27,7 +27,7 @@ get_icon () {
   fi
 }
 
-get_vol () {
+get_vol() {
   local percent=$(get_percentage)
   echo $percent | tr -d '%'
 }
@@ -45,7 +45,7 @@ if [[ $1 == "--vol" ]]; then
 fi
 
 if [[ $1 == "--toggle-muted" ]]; then
-	pamixer -t
+  pamixer -t
 fi
 
 if [[ $1 == "set" ]]; then
